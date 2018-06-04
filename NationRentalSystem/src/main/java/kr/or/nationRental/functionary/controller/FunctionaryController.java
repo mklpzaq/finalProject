fcb3f5f1e98ee0b09e3577067815ae9e5a421f3c
smart-202-15 <smart-202-15@smart-202-15-PC>
2018-06-04 17:28:03 +0900

    [이인호]펑셔너리인서트 작성중

--- NationRentalSystem/src/main/java/kr/or/nationRental/functionary/controller/FunctionaryController.java
+++ NationRentalSystem/src/main/java/kr/or/nationRental/functionary/controller/FunctionaryController.java
@@ -3,6 +3,7 @@ package kr.or.nationRental.functionary.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FunctionaryController {
@@ -10,6 +11,12 @@ public class FunctionaryController {
 	@RequestMapping(value="/insertFunctionary", method=RequestMethod.GET)
 	public String insertFunctionary() {
 			
-		return "";
+		return "/functionary/insertFunctionaryForm";
+	}
+	
+	@RequestMapping(value="/insertFunctionary", method=RequestMethod.POST)
+	public String insertFunctionary(@RequestParam(value="") String id) {
+			
+		return "redirect:/";
 	}
 }
