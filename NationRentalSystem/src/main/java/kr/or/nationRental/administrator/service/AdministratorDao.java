ebec36a64fd22fd43bee9af50903b433207fecf2
smart-202-5 <smart-202-5@smart-202-5-PC>
2018-06-04 17:29:01 +0900

    [임가현}

--- NationRentalSystem/src/main/java/kr/or/nationRental/administrator/service/AdministratorDao.java
+++ NationRentalSystem/src/main/java/kr/or/nationRental/administrator/service/AdministratorDao.java
@@ -1,5 +1,21 @@
 package kr.or.nationRental.administrator.service;
 
-public class AdministratorDao {
+import org.mybatis.spring.SqlSessionTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public class AdministratorDao {
+	@Autowired
+	private SqlSessionTemplate sqlsession;
+	final String NS = "kr.or.nationRental.administrator.service.AdministratorMapper.";
+	
+	private static final Logger logger = LoggerFactory.getLogger(AdministratorDao.class);
+	
+	public int insertAdmin(Administrator administrator) {
+		int row = sqlsession.insert(NS+"insertAdministrator", administrator);	
+		return row;
+	}
 }
