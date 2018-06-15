6054615c62b9f150073a5349f9eec43da13eecb1
smart-202-21 <smart-202-21@smart-202-21-PC>
2018-06-15 17:16:42 +0900

    [이춘림] 안올라가서 다시함

--- NationRentalSystem/src/main/java/kr/or/nationRental/board/service/BoardDao.java
+++ NationRentalSystem/src/main/java/kr/or/nationRental/board/service/BoardDao.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class BoardDao {
-	@Autowired
+	@Autowired 
 	private SqlSessionTemplate sqlSession;
 	private static final Logger logger = LoggerFactory.getLogger(BoardDao.class);
 	final String NS = "kr.or.nationRental.board.service.BoardMapper.";
