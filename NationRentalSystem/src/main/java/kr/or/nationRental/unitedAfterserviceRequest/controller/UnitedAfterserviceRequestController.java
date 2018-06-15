2f040e7f540fc4e25dd003bd008feae4305e7b2b
smart-202-21 <smart-202-21@smart-202-21-PC>
2018-06-15 10:35:35 +0900

    [이춘림] navbar 경로 추가 / UnitedAfterserviceRequestController.java 추가

---
+++ NationRentalSystem/src/main/java/kr/or/nationRental/unitedAfterserviceRequest/controller/UnitedAfterserviceRequestController.java
@@ -0,0 +1,8 @@
+package kr.or.nationRental.unitedAfterserviceRequest.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UnitedAfterserviceRequestController {
+	
+}
